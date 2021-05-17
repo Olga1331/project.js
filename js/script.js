96f1b394688480cf56dc29f0b97401ec749c380c
Olga <chadaeva88@gmail.com>
2021-05-17 13:38:57 +0300

    add numberOfFilms, personalMovieDB

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
-let number = 4.6;
+'use strict';
 
-console.log(4/0);
+const numberOfFilms = +prompt('Сколько фильмов вы уже смотрели?','');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt ('Один из последних просмотренных фильмов?', ''),
+      b = prompt ('На сколько оцените его?', ''),
+      c = prompt ('Один из последних просмотренных фильмов?', ''),
+      d = prompt ('На сколько оцените его?', '');
 
-const person = "Alex";
-
-const bool = true;
-
-console.log('string'*9);
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+      
+      console.log(personalMovieDB);
 
 
 
